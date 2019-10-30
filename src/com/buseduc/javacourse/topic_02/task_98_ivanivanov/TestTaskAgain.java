b3825a305e8392cc5ea1f2c24109973be7f5fe44
Vadim Semenov <vsemenov@leongaming.com>
2019-10-30 23:43:46 +0200

    fix add description

--- src/com/buseduc/javacourse/topic_02/task_98_ivanivanov/TestTaskAgain.java
+++ src/com/buseduc/javacourse/topic_02/task_98_ivanivanov/TestTaskAgain.java
@@ -3,7 +3,10 @@ package com.buseduc.javacourse.topic_02.task_98_ivanivanov;
 /**
  * @author ivanivanov
  * Тема 1. ОСНОВЫ ООП
+ * Задание 98.
+ * Приветствовать любого пользователя при вводе его имени через командную строку.
  */
 
 public class TestTaskAgain {
+    
 }
