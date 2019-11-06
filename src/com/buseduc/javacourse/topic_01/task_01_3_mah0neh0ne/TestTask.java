8854f0101486e281a756eef2e489e655b453abb8
mah0ne <materepti@gmail.com>
2019-11-06 21:13:28 +0200

    prax_mah0neh0ne_topic01

---
+++ src/com/buseduc/javacourse/topic_01/task_01_3_mah0neh0ne/TestTask.java
@@ -0,0 +1,19 @@
+package com.buseduc.javacourse.topic_01.task_01_3_mah0neh0ne;
+
+
+/**
+ * @author mah0neh0ne
+ * Тема 1. Основы ООП
+ * Задание 1_3. Вывести заданное количество случайных чисел.
+ */
+
+public class TestTask {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        for (int i : a) {
+            System.out.println(i);
+            System.out.print(i);
+        }
+    }
+}
