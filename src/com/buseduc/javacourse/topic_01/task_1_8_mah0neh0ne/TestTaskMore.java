1089708da44be7fe4a06cba0493462715d0fcfba
mah0ne <materepti@gmail.com>
2019-11-06 23:47:52 +0200

    prax_mah0neh0ne_topic01_1-8

---
+++ src/com/buseduc/javacourse/topic_01/task_1_8_mah0neh0ne/TestTaskMore.java
@@ -0,0 +1,20 @@
+package com.buseduc.javacourse.topic_01.task_1_8_mah0neh0ne;
+
+/**
+ * @author mah0neh0ne
+ * Тема 1. Основы ООП
+ * Задание 1_8. Вывести числа, которые делятся на 3 или на 9  из n целых чисел..
+ */
+
+public class TestTaskMore {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=100; i++) {    // Задал числа которые делятся от 1 до 100
+            if (i % 3 == 0)
+                if (i % 9 == 0)
+                    System.out.print(i + " ");
+        }
+    }
+}
+
+
