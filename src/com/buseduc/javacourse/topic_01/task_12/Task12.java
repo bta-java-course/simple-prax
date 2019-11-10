6b83bb7c86e0c2234288bfa0a62c9dfc0cf079d9
MariiaLavander <mariia.slipchuk@gmail.com>
2019-11-10 13:23:00 +0200

    Class Main.

---
+++ src/com/buseduc/javacourse/topic_01/task_12/Task12.java
@@ -0,0 +1,14 @@
+package com.buseduc.javacourse.topic_01.task_12_MariiaZakharova21;
+
+/**
+ * @author MariiaZakharova21
+ * Тема 1. ОСНОВЫ ООП
+ * Задание 12
+ * Из n целых чисел вывести все трехзначные числа, в записи которых нет одинаковых цифр.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
