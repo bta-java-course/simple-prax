7270d80207113cd11408ae51f283a2b008cf20fa
MariiaLavander <mariia.slipchuk@gmail.com>
2019-11-10 12:49:50 +0200

    Class Main.

---
+++ src/com/buseduc/javacourse/topic_01/task_05/Task05.java
@@ -0,0 +1,13 @@
+package com.buseduc.javacourse.topic_01.task_09_be78ar;
+
+/**
+ * @author MariiaZakharova21
+ * Тема 1. ОСНОВЫ ООП
+ * Задание 1.5
+ * Вывести суммы произведения n целых чисел.
+ */
+public class Task05 {
+    public static void main(String[] args) {
+
+    }
+}
