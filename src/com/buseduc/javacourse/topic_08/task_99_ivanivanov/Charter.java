14bb36e0a908e99664343c12a26d34da2f8a6101
Vadim Semenov <vsemenov@leongaming.com>
2019-11-18 17:56:31 +0200

    Simple prax examples

---
+++ src/com/buseduc/javacourse/topic_08/task_99_ivanivanov/Charter.java
@@ -0,0 +1,6 @@
+package com.buseduc.javacourse.topic_08.task_99_ivanivanov;
+
+public interface Charter {
+
+    boolean hasContractWithTourOp();
+}
---
+++ src/com/buseduc/javacourse/topic_08/task_99_ivanivanov/Plane.java
@@ -0,0 +1,7 @@
+package com.buseduc.javacourse.topic_08.task_99_ivanivanov;
+
+public abstract class Plane {
+    public void toLand() {
+        System.out.println("Plane is landing!");
+    }
+}
