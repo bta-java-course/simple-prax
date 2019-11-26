4bb9d50cd23f6d928b92102116660666e7d8be72
MariiaLavander <mariia.slipchuk@gmail.com>
2019-11-26 23:32:30 +0200

    Complete topic 2, task 5.

--- src/com/buseduc/javacourse/topic_02/task_5_mariiazakharova21/Main.java
+++ src/com/buseduc/javacourse/topic_02/task_5_mariiazakharova21/Main.java
@@ -1,6 +1,7 @@
 package com.buseduc.javacourse.topic_02.task_5_mariiazakharova21;
 
 import com.buseduc.javacourse.utils.InputUtils;
+import javafx.util.Pair;
 
 /**
  * @author MariiaZakharova21
@@ -14,20 +15,47 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = InputUtils.getNumbersFromInput();
-
+        int a = 0;
+        int b = 0;
         for (int n : numbers
-             ) {
-            System.out.println(n);
+        ) {
+            List<Integer> digits = getDigits(n);
+            Numbers x = compute(digits);
+
+            if (x.odd == 0) {
+                a++;
+            }
+
+            if (x.even == x.odd) {
+                b++;
+            }
+
         }
+        System.out.println(a);
+        System.out.println(b);
     }
 
-    public static List<Integer> getDigits(int number){
+    public static List<Integer> getDigits(int number) {
         List<Integer> digits = new ArrayList<Integer>();
-        while (number != 0){
+        while (number != 0) {
             int x = number % 10;
             digits.add(x);
             number = number / 10;
         }
         return digits;
+    }
+
+    public static Numbers compute(List<Integer> digits) {
+        int countOdd = 0;
+        int countEven = 0;
+        for (int d : digits
+        ) {
+            if (d % 2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+        }
+        return new Numbers(countEven, countOdd);
     }
 }
---
+++ src/com/buseduc/javacourse/topic_02/task_5_mariiazakharova21/Numbers.java
@@ -0,0 +1,11 @@
+package com.buseduc.javacourse.topic_02.task_5_mariiazakharova21;
+
+public class Numbers {
+    int even;
+    int odd;
+
+    Numbers(int even, int odd) {
+        this.even = even;
+        this.odd = odd;
+    }
+}
