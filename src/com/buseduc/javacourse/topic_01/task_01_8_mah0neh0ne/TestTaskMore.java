f95cdae0674e04eb71be261bd25c48f3a18427f9
mah0ne <materepti@gmail.com>
2019-11-27 20:29:02 +0200

    topic_01/task_01_8_mah0neh0ne_ispravil eshe raz

--- src/com/buseduc/javacourse/topic_01/task_01_8_mah0neh0ne/TestTaskMore.java
+++ src/com/buseduc/javacourse/topic_01/task_01_8_mah0neh0ne/TestTaskMore.java
@@ -1,5 +1,9 @@
 package com.buseduc.javacourse.topic_01.task_01_8_mah0neh0ne;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author mah0neh0ne
  * Тема 1. Основы ООП
@@ -7,14 +11,25 @@ package com.buseduc.javacourse.topic_01.task_01_8_mah0neh0ne;
  */
 
 public class TestTaskMore {
-
-    public static void main(String[] args) {
-        for (int i = 1; i <=100; i++) {    // Задал числа которые делятся от 1 до 100
-            if (i % 3 == 0)
-                if (i % 9 == 0)
-                    System.out.print(i + " ");
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Для прекращения ввода введите нечисленное значение или число между -45 и 45");
+            String hint = "Введите целое число: ";
+            System.out.println(hint);
+            List<Long> nums = new ArrayList<>();
+            while (scanner.hasNextLong()) {  //program will work, while input is integer or long integer
+                long i = scanner.nextLong();
+                if (i >-45 && i < 45) {
+                    System.out.println("Эмм... этот диапазон чисел ТОЧНО не делится и на 3 и на 9. Забыл таблицу умножения?");
+                    System.out.println("Task over");
+                    break;
+                } else if (i % 45 == 0)
+                    nums.add(i);
+            }
+            System.out.println("числа делящиеся на 3 и 9 : " + nums);
+            System.out.println("Task over");
         }
     }
-}
+
 
 
