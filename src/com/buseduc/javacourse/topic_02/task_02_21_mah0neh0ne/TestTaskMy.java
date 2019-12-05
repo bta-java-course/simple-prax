cc74bb6c721ef86dfd337b90f6e26e473b3bea45
mah0ne <materepti@gmail.com>
2019-12-05 18:15:56 +0200

    ispravil

--- src/com/buseduc/javacourse/topic_02/task_02_21_mah0neh0ne/TestTaskMy.java
+++ src/com/buseduc/javacourse/topic_02/task_02_21_mah0neh0ne/TestTaskMy.java
@@ -16,50 +16,26 @@ import java.util.Random;
 public class TestTaskMy {
     public static void main(String[] args) {
         int[][] a = new int[4][4];
-        int max, min;
-        Random rnd = new Random();
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                a[i][j] = rnd.nextInt(4) + 0;
-            }
-        }
-        for (int i = 0; i < a.length; i++, System.out.println()) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-        }
-        int countZero = 0;
-        int n = 0;
-        double[][] sourceNumbers = new double[0][];
-        for (int i1 = 0; i1 < n; i1++){
-            for (int j1 = i1; j1 < n; j1++){
-                if(sourceNumbers[i1][j1] == 0) {
-                    countZero++;
-                    for (int i2 = i1; i2 < n; i2++){
-                        for (int j2 = j1; j2 < n; j2++){
-                            if(j2 + 1 == n && i2 < n){
-                                // смещение первого элемента след. строки
-                                int p1 = i2 + 1;
-                                int p2 = 0;
-                                sourceNumbers[i2][j2] = sourceNumbers[p1][p2];
-                            }
-                            else
-                                sourceNumbers[i2][j2] = sourceNumbers[i2][j2+1];
-                        }
-                        j1 = 0;// если 0 был не в начале строки, со следующей строки j обнуляем
-                    }
-                    sourceNumbers[n -1][n -1-countZero] = 0; // вставка нулей в последнюю строку
+            Random rnd = new Random();
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[i].length; j++) {
+                    a[i][j] = rnd.nextInt(4);
                 }
+                System.out.println(Arrays.toString(a[i]));
             }
-        }
-        for (int i = 0; i < sourceNumbers.length; i++) {
             System.out.println();
-            for (int j = 0; j < sourceNumbers[i].length; j++) {
-                System.out.print(sourceNumbers[i][j] + "\t");
+
+            for (int i = 0; i < a.length; i++) {
+                int[][] res = new int[a.length][a[i].length];
+                int index = 0;
+                for (int j = 0; j < a[i].length; j++) {
+                    if (a[i][j] != 0) res[i][index++] = a[i][j];
+                }
+                System.out.println(Arrays.toString(res[i]));
             }
         }
     }
-    }
+
 
 
 
