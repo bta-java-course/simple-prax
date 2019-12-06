2bd2a5e97db0b7c3c3a07caa6ccaf6f19ec38d1f
Alex Snigur <aleksei.snigur@mail.ee>
2019-12-06 15:54:41 +0200

    #topic_08_Task_08_06_AlexSnigur, completed

---
+++ src/com/buseduc/javacourse/topic_08/task_08_06_AlexSnigur/Ship.java
@@ -0,0 +1,8 @@
+package com.buseduc.javacourse.topic_08.task_08_06_AlexSnigur;
+
+public abstract class Ship {
+
+    public void toFloat() {
+        System.out.println("Корабль идёт по морю ");
+    }
+}
