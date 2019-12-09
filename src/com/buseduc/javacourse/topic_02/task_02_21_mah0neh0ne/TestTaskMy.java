547437f7bcdb70b3a591ee984b1df967281a1e46
mah0ne <materepti@gmail.com>
2019-12-09 19:32:34 +0200

    vse zadanija vqpolnenq

--- src/com/buseduc/javacourse/topic_02/task_02_21_mah0neh0ne/TestTaskMy.java
+++ src/com/buseduc/javacourse/topic_02/task_02_21_mah0neh0ne/TestTaskMy.java
@@ -9,21 +9,22 @@ import java.util.Random;
  * @author mah0neh0ne
  * Тема 2. Типы и Операторы
  * Задание 2_21. Создать таблицу чисел (4 Х 4) со случайными значениями от 0 до 3 и вывести ее на консоль.
-Преобразовать строки таким образом, чтобы элементы, равные нулю, располагались после всех остальных.
+ *Преобразовать строки таким образом, чтобы элементы, равные нулю, располагались после всех остальных.
  */
 
 
 public class TestTaskMy {
     public static void main(String[] args) {
         int[][] a = new int[4][4];
-            Random rnd = new Random();
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < a[i].length; j++) {
-                    a[i][j] = rnd.nextInt(4);
-                }
-                System.out.println(Arrays.toString(a[i]));
+        Random rnd = new Random();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = rnd.nextInt(4);
             }
-            System.out.println();
+            System.out.println(Arrays.toString(a[i]));
+        }
+
+        System.out.println();
 
             for (int i = 0; i < a.length; i++) {
                 int[][] res = new int[a.length][a[i].length];
@@ -35,6 +36,7 @@ public class TestTaskMy {
             }
         }
     }
+
 
 
 
